a33cfd354f482721da7f9ff27bb89a510e04d196
Sacharomyces <johndoe@example.cob.mackiewicz1@gmail.com>
2017-05-17 17:09:51 +0200

    Searchbox added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
+
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+      <input class="form-control" type="search" [formControl]="searchControl">
+    `
 })
+
 export class AppComponent {
-  title = 'app works!';
+
+  searchControl = new FormControl();
+
+  constructor() {
+    this.searchControl.valueChanges
+        .filter(text => text.length >= 3)
+        .debounceTime(400)
+        .distinctUntilChanged()
+        .subscribe(value => {console.log(value);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,9 +11,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
